3ac602ba9be3af38d083e4057d353fb208796ab8
Norisama <gloriazbh@gmail.com>
2019-10-06 17:54:26 -0400

    commit all test cases

--- tests/testthat/test-ringPlot.R
+++ tests/testthat/test-ringPlot.R
@@ -1,3 +1,30 @@
-test_that("multiplication works", {
+
+
+
+
+test_that("getRingRadius works", {
+  getRingRadius_test <- getRingRadius(ring_table_data = Table25, tree_outer_radius = 3)
+  factor <- c("vf1","vf2")
+  ring_radius <- c(4,5)
+  getRingRadius_expected <-data.frame(factor,ring_radius)
+
+  expect_equal(getRingRadius_test$ring_radius[1], getRingRadius_expected$ring_radius[1])
+  expect_equal(getRingRadius_test$ring_radius[2], getRingRadius_expected$ring_radius[2])
+})
+
+
+test_that("getRingData works", {
+
+  getRingData_expected <- circleFun(center = c(0,0), r = 6 , npoints = 6)
+  getRingData_test <- getRingData(table_data = Table6 , tree_depth = 3, ring_radius = 6 )
+
+  expect_equal(getRingData_test$x, getRingData_expected$x)
+  expect_equal(getRingData_test$y, getRingData_expected$y)
+})
+
+
+test_that("ringPlot works", {
+
   expect_equal(2 * 2, 4)
 })
+
--- tests/testthat/test-treePlot.R
+++ tests/testthat/test-treePlot.R
@@ -7,44 +7,56 @@
 
 test_that("treeInputProcess works", {
 
-  expect_equal()
+  treeInputProcess_test <- treeInputProcess(tree = Tree100)
+
+  expect_equal(treeInputProcess_test$depth[101],0)
+  expect_equal(treeInputProcess_test$depth_length [101],0)
+  expect_equal(max(treeInputProcess_test$depth),14)
+  expect_equal(treeInputProcess_test$angle[1] ,0)
+  expect_equal(treeInputProcess_test$parent[1],109)
+  expect_equal(treeInputProcess_test$c1[101] ,100)
 
 })
 
 test_that("circleFun works", {
   x <- c(1.000000e+01,6.123234e-16,-1.000000e+01,-1.836970e-15,1.000000e+01)
   y <- c(0.000000e+00,1.000000e+01,1.224647e-15,-1.000000e+01,-2.449294e-15)
-  try5 <- data.frame(x,y)
+  circleFun_expected <- data.frame(x,y)
 
-  x1 <- c(10,0)
-  y1 <- c(10,-2.449294e-15)
+  expect_equal(circleFun(npoints = 5), circleFun_expected)
 
-  expect_equal(circleFun(npoints = 5), try5 )
-  expect_equal(circleFun(npoints = 2), try2 )
 })
 
 test_that("getNPoints works", {
 
-  expect_equal()
+  getNPoints_test1 <- getNPoints( ntips = 100)
+  getNPoints_test2 <- getNPoints( ntips = 200)
+  getNPoints_test3 <- getNPoints( ntips = 20,refine_factor = 30)
+
+  expect_equal(getNPoints_test1, 10000)
+  expect_equal(getNPoints_test2, 20000)
+  expect_equal(getNPoints_test3, 600)
 })
 
 test_that("getLayers works", {
+  getLayers_test <- getLayers(data = treeInputProcess_test,npoint = getNPoints_test1 , ntips = 100)
 
-  expect_equal()
+  expect_equal(max(getLayers_test$layer_id), 14)
+  expect_equal(nrow(getLayers_test),13 * 10000 + 100 + 14)
+  expect_equal(sum(getLayers_test$x), 105)
 })
 
 test_that("getCoordinates works", {
 
-  expect_equal()
+  getCoordinates_test <- getCoordinates(tree = treeInputProcess_test, layers = getLayers_test, npoint = getNPoints_test1)
+  expect_equal(nrow(getCoordinates_test),199)
+  expect_equal(ncol(getCoordinates_test),11)
+
+  tips_test<-filter(getLayers_test,getLayers_test$layer_id == max(getLayers_test$layer_id))
+  tips_expected <- filter(getCoordinates_test,getCoordinates_test$c1 == -1)
+
+  expect_equal(tips_test$x[1:100], tips_expected$x[1:100])
+  expect_equal(nrow(getCoordinates_test),199)
 })
 
-test_that("nodeGroup works", {
-
-  expect_equal()
-})
-
-test_that("treePlot works", {
-
-  expect_equal()
-})
 
